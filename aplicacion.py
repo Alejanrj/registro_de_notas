54fa0578301ef5d475384e6fd8facf65c977e820
Alejanrj <Alejanrj.07@gmail.com>
2026-03-04 19:24:31 -0500

    ajuste de mensajes en rama de prueba

--- aplicacion.py
+++ aplicacion.py
@@ -1,37 +1,58 @@
-def main():
 
-    base_datos = []
+base_datos = []
 
-    while True:
-        print("\n--- SISTEMA DE REGISTRO DE NOTAS ---")
-        print("1. Registrar ingreso (Nombre)")
-        print("2. Registrar nota")
-        print("3. Ver promedio")
-        print("4. Salir")
+def registrar_estudiante():
+    nombre = input("Nombre del estudiante: ")
+    # diccionario sencillo
+    estudiante = {"nombre": nombre, "nota": 0.0}
+    base_datos.append(estudiante)
+    print("Estudiante guardado con éxito.")
+
+
+def poner_nota():
+    busqueda = input("¿Nota que deseas registrar?: ")
+    for persona in base_datos:
+        if persona["nombre"].lower() == busqueda.lower():
+            nueva_nota = float(input("Ingresa la nota: "))
+            persona["nota"] = nueva_nota
+            print("Nota registrada.")
+            return
+    print("No encontré a ese estudiante.")
+
+def calcular_promedio():
+    if not base_datos: 
+        print("No hay nadie en la lista.")
+    else:
+        suma = 0
+        cantidad = 0
+        for persona in base_datos:
+            suma = suma + persona["nota"]
+            cantidad = cantidad + 1
         
-        opcion = input("Selecciona una opción (1-4): ")
+        promedio = suma / cantidad
         
-        if opcion == '1':
-            nombre = input("Ingresa el nombre del estudiante: ")
-            estudiante = {
-                "nombre": nombre,
-                "nota": 0.0
-            }
-        
-            base_datos.append(estudiante)
-            print(f" Estudiante '{nombre}' registrado con éxito.")
-            
-        elif opcion == '2':
-            print("\n[Opción 2 ]")
-            
-        elif opcion == '3':
-            print("\n[Opción 3 ]")
-            
-        elif opcion == '4':
-            print("Saliendo... ")
-            break
+        if cantidad == 1:
+            nombre_unico = base_datos[0]['nombre']
+            print(f"La nota de {nombre_unico} es: {promedio}")
         else:
-            print(" Opción no válida.")
+            print(f"El promedio de los {cantidad} estudiantes es: {promedio}")
 
-if __name__ == "__main__":
-    main()
+while True:
+    print("\n--- MENÚ DE NOTAS ---")
+    print("1. Registrar Estudiante")
+    print("2. Registrar Nota")
+    print("3. Ver Promedio")
+    print("4. Salir")
+    
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        registrar_estudiante() 
+    elif opcion == "2":
+        poner_nota()          
+    elif opcion == "3":
+        calcular_promedio()    
+    elif opcion == "4":
+        print("¡Hasta luego!")
+        break
+    # Esta es una línea de prueba en la rama feature/prueba
